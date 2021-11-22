8c6a1c886cfbb94d8ad570ac8cb5ee3403a1b053
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-11-22 14:28:10 +0100

    Problem30

--- src/com/company/Problem30.java
+++ src/com/company/Problem30.java
@@ -9,7 +9,7 @@ public class Problem30 {
 
     public void solution(){
         int sum = 0;
-        for(int i=2;i<10000000;i++){
+        for(int i=2;i<1_000_000;i++){
             if(check(i)){
                 sum+=i;
                 System.out.println(i);
@@ -21,7 +21,7 @@ public class Problem30 {
     public boolean check(int i){
         int s = 0;
         for(char c : String.valueOf(i).toCharArray()){
-            s+= Math.pow(Integer.valueOf(""+c),5);
+            s+= Math.pow(Integer.parseInt(""+c),5);
         }
         return s ==i;
     }
