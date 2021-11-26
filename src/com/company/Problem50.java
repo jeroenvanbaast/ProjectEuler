e0025e0ff43a38c28563abcfc84fdf2d97e58892
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-11-26 11:43:36 +0100

    Problem 50

---
+++ src/com/company/Problem50.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Problem50 {
+
+    public static void main(String[] args) {
+        Problem50 p = new Problem50();
+        p.solution();
+    }
+
+    private void solution() {
+        ArrayList<Integer> primes = primes(1_000_000);
+        int highestPrime = primes.get(primes.size() - 1);
+        int size = 3;
+        int a = 0;
+        while (size < primes.size()) {
+            for (int i = 0; i < primes.size() - size; i++) {
+                int sum = 0;
+                for (int j = 0; j < size; j++) {
+                    sum += primes.get(i + j);
+                }
+                if (sum > highestPrime) {
+                    if(i==0){
+                        System.out.println(a);
+                        return;
+                    }
+                    size++;
+                    break;
+                } else if (primes.contains(sum)) {
+                    a = sum;
+                    size++;
+                    break;
+                }
+            }
+        }
+        System.out.println(a);
+    }
+
+
+    public ArrayList<Integer> primes(int n) {
+        boolean[] notPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i]) {
+                continue;
+            }
+            for (int j = i + i; j < n; j += i) {
+                notPrime[j] = true;
+            }
+
+        }
+        ArrayList<Integer> primes = new ArrayList<>();
+        for (int i = 2; i < notPrime.length; i++) {
+            if (!notPrime[i]) {
+                primes.add(i);
+            }
+        }
+        return primes;
+    }
+
+    public boolean isPrime(long d) {
+        if (d <= 1) {
+            return false;
+        }
+        if (d == 2) {
+            return true;
+        }
+        for (int i = 2; i < d - 1; i++) {
+            if (d % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
