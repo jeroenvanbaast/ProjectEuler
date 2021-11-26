26fac948817a54cee43b3af7115d6518b10629f1
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-11-26 14:59:53 +0100

    Problem 53

--- src/com/company/Problem53.java
+++ src/com/company/Problem53.java
@@ -11,14 +11,16 @@ public class Problem53 {
     }
 
     private void solution() {
+        BigInteger[] fac = new BigInteger[101];
+        for(int i = 0; i<=100;i++){
+            fac[i] = factorial(BigInteger.valueOf(i));
+        }
         int count =0;
         for (int n = 1; n <= 100; n++) {
             for (int r = 0; r <= n; r++) {
-                BigInteger a = factorial(BigInteger.valueOf(n));
-                BigInteger b = factorial(BigInteger.valueOf(r));
-                if(a.divide(b.multiply(factorial(BigInteger.valueOf(n - r)))).compareTo(BigInteger.valueOf(1_000_000)) > 0){
+                if(fac[n].divide(fac[r].multiply(fac[n-r])).compareTo(BigInteger.valueOf(1_000_000)) > 0){
                     count++;
-                } 
+                }
             }
         }
         System.out.println("Solution: " + count);
