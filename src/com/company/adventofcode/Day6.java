2e935992a73ae24fe2908264c756c04d5e4ec6e8
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-12-06 09:25:12 +0100

    Day6

--- src/com/company/adventofcode/Day6.java
+++ src/com/company/adventofcode/Day6.java
@@ -1,49 +1,57 @@
 package com.company.adventofcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Day6 {
 
     public static void main(String[] args) {
         Day6 day = new Day6();
-        List<Integer> test = new ArrayList<>(Arrays.asList(3,4,3,1,2));
-        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,4,5,5,5,2,1,3,1,4,3,2,1,5,5,1,2,3,4,4,1,2,3,2,1,4,4,1,5,5,1,3,4,4,4,1,2,2,5,1,5,5,3,2,3,1,1,3,5,1,1,2,4,2,3,1,1,2,1,3,1,2,1,1,2,1,2,2,1,1,1,1,5,4,5,2,1,3,2,4,1,1,3,4,1,4,1,5,1,4,1,5,3,2,3,2,2,4,4,3,3,4,3,4,4,3,4,5,1,2,5,2,1,5,5,1,3,4,2,2,4,2,2,1,3,2,5,5,1,3,3,4,3,5,3,5,5,4,5,1,1,4,1,4,5,1,1,1,4,1,1,4,2,1,4,1,3,4,4,3,1,2,2,4,3,3,2,2,2,3,5,5,2,3,1,5,1,1,1,1,3,1,4,1,4,1,2,5,3,2,4,4,1,3,1,1,1,3,4,4,1,1,2,1,4,3,4,2,2,3,2,4,3,1,5,1,3,1,4,5,5,3,5,1,3,5,5,4,2,3,2,4,1,3,2,2,2,1,3,4,2,5,2,5,3,5,5,1,1,1,2,2,3,1,4,4,4,5,4,5,5,1,4,5,5,4,1,1,5,3,3,1,4,1,3,1,1,4,1,5,2,3,2,3,1,2,2,2,1,1,5,1,4,5,2,4,2,2,3));
-//        day.solution(test, 18);
-        day.calc(test,18);
+        List<Integer> test = new ArrayList<>(Arrays.asList(3, 4, 3, 1, 2));
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 4, 5, 5, 5, 2, 1, 3, 1, 4, 3, 2, 1, 5, 5, 1, 2, 3, 4, 4, 1, 2, 3, 2, 1, 4, 4, 1, 5, 5, 1, 3, 4, 4, 4, 1, 2, 2, 5, 1, 5, 5, 3, 2, 3, 1, 1, 3, 5, 1, 1, 2, 4, 2, 3, 1, 1, 2, 1, 3, 1, 2, 1, 1, 2, 1, 2, 2, 1, 1, 1, 1, 5, 4, 5, 2, 1, 3, 2, 4, 1, 1, 3, 4, 1, 4, 1, 5, 1, 4, 1, 5, 3, 2, 3, 2, 2, 4, 4, 3, 3, 4, 3, 4, 4, 3, 4, 5, 1, 2, 5, 2, 1, 5, 5, 1, 3, 4, 2, 2, 4, 2, 2, 1, 3, 2, 5, 5, 1, 3, 3, 4, 3, 5, 3, 5, 5, 4, 5, 1, 1, 4, 1, 4, 5, 1, 1, 1, 4, 1, 1, 4, 2, 1, 4, 1, 3, 4, 4, 3, 1, 2, 2, 4, 3, 3, 2, 2, 2, 3, 5, 5, 2, 3, 1, 5, 1, 1, 1, 1, 3, 1, 4, 1, 4, 1, 2, 5, 3, 2, 4, 4, 1, 3, 1, 1, 1, 3, 4, 4, 1, 1, 2, 1, 4, 3, 4, 2, 2, 3, 2, 4, 3, 1, 5, 1, 3, 1, 4, 5, 5, 3, 5, 1, 3, 5, 5, 4, 2, 3, 2, 4, 1, 3, 2, 2, 2, 1, 3, 4, 2, 5, 2, 5, 3, 5, 5, 1, 1, 1, 2, 2, 3, 1, 4, 4, 4, 5, 4, 5, 5, 1, 4, 5, 5, 4, 1, 1, 5, 3, 3, 1, 4, 1, 3, 1, 1, 4, 1, 5, 2, 3, 2, 3, 1, 2, 2, 2, 1, 1, 5, 1, 4, 5, 2, 4, 2, 2, 3));
+        day.test(day.createMap(list), 256);
     }
 
-    private void solution(List<Integer> list, int days) {
-        if(days ==0){
-            System.out.println("Solution: " + list.size());
-            System.out.println(list);
+    private HashMap<Long, Long> createMap(List<Integer> list) {
+        HashMap<Long, Long> map = new HashMap<>();
+        for (Integer j : list) {
+            Long i = (long)j;
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1L);
+            }
+        }
+        return map;
+    }
+
+    private void test(HashMap<Long, Long> map, int days) {
+        if (days == 0) {
+            Long count =0L;
+            for(Long v : map.values()){
+                count+=v;
+            }
+            System.out.println("Solution: " + count);
             return;
         }
-        int toAdd =0;
-        for(int i=0; i< list.size();i++){
-            if(list.get(i)==0){
-                toAdd++;
-                list.set(i,6);
-            }else{
-                list.set(i,list.get(i)-1);
-            }
-        }
-        for(int i =0; i< toAdd; i++){
-            list.add(8);
-        }
-        solution(list,days-1);
-    }
+        HashMap<Long, Long> coppy = new HashMap<>();
+        for (Map.Entry<Long, Long> e : map.entrySet()) {
+            Long key = e.getKey();
+            if (key == 0L) {
+                coppy.put(8L, e.getValue());
+                if (coppy.containsKey(6L)) {
+                    coppy.put(6L, coppy.get(6L) + e.getValue());
+                } else {
+                    coppy.put(6L, e.getValue());
+                }
+            } else if (map.containsKey(key)) {
+                if (coppy.containsKey(key-1)) {
+                    coppy.put(key - 1, coppy.get(key-1) + e.getValue());
+                }else{
+                    coppy.put(key - 1, e.getValue());
 
-    private void calc(List<Integer> list, int days){
-        int a = days%7;
-        for(int i=0; i< list.size();i++){
-            int b = list.get(i) -a;
-            if(b < 0){
-                b=7+b;
+                }
             }
-            list.set(i, b);
         }
-        System.out.println(list + " " );
+        test(coppy, days - 1);
     }
 }
