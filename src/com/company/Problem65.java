5adf265820e9acf827eae4b63a68a79ab0524352
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-12-07 11:50:58 +0100

    Problem65

---
+++ src/com/company/Problem65.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Problem65 {
+
+    public static void main(String[] args) {
+        Problem65 p = new Problem65();
+        p.solution(100);
+    }
+
+    private void solution(int n) {
+        BigInteger e = BigInteger.valueOf(8L);
+        BigInteger prevE = BigInteger.valueOf(3);
+        int times = 4;
+        for (int i = 1; i < n-2; i++) {
+            BigInteger tmp = e;
+            if (i % 3 == 0) {
+                e = e.multiply(BigInteger.valueOf(times)).add(prevE);
+                prevE = tmp;
+                times += 2;
+            } else {
+                e = e.add(prevE);
+                prevE = tmp;
+            }
+        }
+        System.out.println("Solution: " +
+                e.toString().chars().map(c -> Integer.parseInt(String.valueOf((char) c))).sum());
+    }
+}
