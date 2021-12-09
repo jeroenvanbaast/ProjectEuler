f06189524651a3f13ac736ded58f30067be15ddc
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-12-09 16:49:07 +0100

    Golfing

--- src/com/company/adventofcode/Day9.java
+++ src/com/company/adventofcode/Day9.java
@@ -19,38 +19,34 @@ public class Day9 {
     }
 
     private void solution(int[][] input) {
-        int basinCount = 0;
         for (int i = 0; i < input.length; i++) {
             for (int j = 0; j < input[0].length; j++) {
                 int current = input[i][j];
                 if (current!=-1 && current!= 9) {
-                    basins.add(0);
-                    basinSize(input, i, j, basinCount++);
+                    basins.add(basinSize(input, i, j, 0));
                 }
             }
         }
         System.out.println("Solution: " + basins.stream().sorted(Collections.reverseOrder()).limit(3).mapToInt(s -> s).reduce(1, (a, b) -> a * b));
     }
 
-    private void basinSize(int[][] map, int i, int j, int index) {
-        if (map[i][j] == -1) {
-            return;
-        }
-        if (map[i][j] != 9) {
-            basins.set(index, basins.get(index) + 1);
+    private int basinSize(int[][] map, int i, int j, int ans) {
+        if (map[i][j] != 9 && map[i][j] != -1) {
+            ++ans;
             map[i][j] = -1;
             if (j != 0) {
-                basinSize(map, i, j - 1, index);
+                ans = basinSize(map, i, j - 1, ans);
             }
             if (j < map[0].length - 1) {
-                basinSize(map, i, j + 1, index);
+                ans = basinSize(map, i, j + 1, ans);
             }
             if (i != 0) {
-                basinSize(map, i - 1, j, index);
+                ans = basinSize(map, i - 1, j, ans);
             }
             if (i < map.length - 1) {
-                basinSize(map, i + 1, j, index);
+                ans = basinSize(map, i + 1, j, ans);
             }
         }
+        return ans;
     }
 }
