6c3c94abf6716cc28ee9c3d26ef2cc86481c3664
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-12-10 09:32:15 +0100

    Day10

--- src/com/company/adventofcode/Day10.java
+++ src/com/company/adventofcode/Day10.java
@@ -4,55 +4,65 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Day10 {
 
     public static void main(String[] args) throws IOException {
-        ArrayList<String> test = new ArrayList<>(Files.readAllLines(Path.of("src/com/company/adventofcode/test.txt")));
         ArrayList<String> input = new ArrayList<>(Files.readAllLines(Path.of("src/com/company/adventofcode/day10.txt")));
-        Day10 d = new Day10();
-        d.solution1(input);
-        d.solution2(input);
+        new Day10().solution(input);
     }
 
-    private void solution2(ArrayList<String> input) {
-    }
-
-    private void solution1(ArrayList<String> test) {
-        ArrayList<String> a = new ArrayList<>();
+    private void solution(ArrayList<String> test) {
         int ans =0;
-        for (String line : test) {
+        ArrayList<Long> scores = new ArrayList<>();
+       outer:  for (String line : test) {
+           ArrayList<String> a = new ArrayList<>();
             for (char c : line.toCharArray()) {
                 String s = "" + c;
                 if (s.equals(")")) {
                     if(!a.get(a.size()-1).equals("(")){
                         ans+=3;
-                        break;
+                        continue outer;
                     }
                     a.remove(a.size()-1);
                 } else if (s.equals("]")) {
                     if(!a.get(a.size()-1).equals("[")){
                         ans+=57;
-                        break;
+                        continue outer;
                     }
                     a.remove(a.size()-1);
                 } else if (s.equals("}")) {
                     if(!a.get(a.size()-1).equals("{")){
                         ans+=1197;
-                        break;
+                        continue outer;
                     }
                     a.remove(a.size()-1);
                 } else if (s.equals(">")) {
                     if(!a.get(a.size()-1).equals("<")){
                         ans+=25137;
-                        break;
+                        continue outer;
                     }
                     a.remove(a.size()-1);
                 } else {
                     a.add(s);
                 }
             }
+            long tmp = 0;
+            for(int i = a.size()-1; i>=0; i--){
+                String s = a.get(i);
+                if (s.equals("(")) {
+                    tmp = tmp*5+1;
+                } else if (s.equals("[")) {
+                    tmp = tmp*5+2;
+                } else if (s.equals("{")) {
+                    tmp = tmp*5+3;
+                } else if (s.equals("<")) {
+                    tmp = tmp*5+4;
+            }}
+            scores.add(tmp);
         }
-        System.out.println("Solution: " + ans);
+        System.out.println("Solution1: " + ans);
+        System.out.println("Solution2: " + scores.stream().sorted().collect(Collectors.toList()).get(scores.size()/2));
     }
 }
