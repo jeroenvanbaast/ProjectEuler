a806700fda19eb154874d2f850286a2dd29a29cc
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-12-13 11:13:43 +0100

    Day13

--- src/com/company/adventofcode/Day13.java
+++ src/com/company/adventofcode/Day13.java
@@ -10,52 +10,40 @@ public class Day13 {
 
     public static void main(String[] args) throws IOException {
         ArrayList<String> input = new ArrayList<>(Files.readAllLines(Path.of("src/com/company/adventofcode/day13.txt")));
-        ArrayList<String> test = new ArrayList<>(Files.readAllLines(Path.of("src/com/company/adventofcode/test.txt")));
-        Day13 day13 = new Day13();
-//        day13.solution(test, 2);
-        day13.solution(input, 12);
+        new Day13().solution(input, 12);
     }
 
     private void solution(ArrayList<String> input, int folds) {
-        ArrayList<String> foldLines = new ArrayList<>(input.subList(input.size()-folds, input.size()));
+        ArrayList<String> foldLines = new ArrayList<>(input.subList(input.size() - folds, input.size()));
         input.removeAll(foldLines);
-        input.remove(input.size()-1);
+        input.remove(input.size() - 1);
         boolean[][] paper = dotPaper(input);
-        for(String fold : foldLines){
+        for (String fold : foldLines) {
             String[] parts = fold.split(" ");
-            if(parts[2].contains("y")){
-                int line = Integer.parseInt(parts[2].substring(2));
+            int line = Integer.parseInt(parts[2].substring(2));
+            if (parts[2].contains("y")) {
                 boolean[][] topHalf = Arrays.copyOfRange(paper, 0, line);
-                boolean[][] botHalf = Arrays.copyOfRange(paper, line+1, paper.length);
-                int j =0;
-                for(int i = topHalf.length-1; i >= 0; i--){
-                    for(int k =0; k < topHalf[0].length; k++){
-                        if(topHalf[i][k] || botHalf[j][k]){
+                boolean[][] botHalf = Arrays.copyOfRange(paper, line + 1, paper.length);
+                for (int i = topHalf.length - 1; i >= 0; i--) {
+                    for (int k = 0; k < topHalf[0].length; k++) {
+                        if (topHalf[i][k] || botHalf[topHalf.length-i-1][k]) {
                             topHalf[i][k] = true;
                         }
                     }
-                    j++;
                 }
                 paper = topHalf;
-            }else{
-                int line = Integer.parseInt(parts[2].substring(2));
+            } else {
                 boolean[][] leftHalf = new boolean[paper.length][line];
-                boolean[][] rightHalf = new boolean[paper.length][paper[0].length - line-1];
-                for(int j = 0; j < paper.length; j++){
-                    for(int i = 0; i < line; i++){
-                        leftHalf[j][i] = paper[j][i];
-                    }
-                    for(int i = line+1; i <= paper[0].length-1; i++){
-                        rightHalf[j][i-line-1] = paper[j][i];
-                    }
+                boolean[][] rightHalf = new boolean[paper.length][paper[0].length - line - 1];
+                for (int i = 0; i < paper.length; i++) {
+                    leftHalf[i] = Arrays.copyOfRange(paper[i], 0, line);
+                    rightHalf[i] = Arrays.copyOfRange(paper[i], line + 1, paper[i].length);
                 }
-                for(int i = 0; i < leftHalf.length; i++){
-                    int j =0;
-                    for(int k =leftHalf[0].length-1; k >= 0; k--){
-                        if(leftHalf[i][k] || rightHalf[i][j]){
+                for (int i = 0; i < leftHalf.length; i++) {
+                    for (int k = leftHalf[0].length - 1; k >= 0; k--) {
+                        if (leftHalf[i][k] || rightHalf[i][leftHalf[0].length-k-1]) {
                             leftHalf[i][k] = true;
                         }
-                        j++;
                     }
                 }
                 paper = leftHalf;
@@ -63,29 +51,29 @@ public class Day13 {
             System.out.println();
         }
         output(paper);
-        int count =0;
-        for(boolean[] bArray : paper){
-            for(boolean b : bArray){
-                count += b ? 1:0;
+        int count = 0;
+        for (boolean[] bArray : paper) {
+            for (boolean b : bArray) {
+                count += b ? 1 : 0;
             }
         }
         System.out.println("Solution1: " + count);
     }
 
 
-    private void output(boolean[][] paper){
-        for(boolean[] bArray : paper){
+    private void output(boolean[][] paper) {
+        for (boolean[] bArray : paper) {
             String tmp = "";
-            for(boolean b : bArray){
-                tmp += b? "#":".";
+            for (boolean b : bArray) {
+                tmp += b ? "#" : " ";
             }
             System.out.println(tmp);
         }
     }
 
-    private boolean[][] dotPaper(ArrayList<String> input){
+    private boolean[][] dotPaper(ArrayList<String> input) {
         boolean[][] paper = new boolean[15000][11000];
-        for(String s : input){
+        for (String s : input) {
             String[] cords = s.split(",");
             paper[Integer.parseInt(cords[1])][Integer.parseInt(cords[0])] = true;
         }
