aa9ecd4e5a1e5a8f5ff125de4bd762c8fc42e1ec
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-12-14 11:51:13 +0100

    day14

--- src/com/company/adventofcode/Day14.java
+++ src/com/company/adventofcode/Day14.java
@@ -4,20 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day14 {
 
     public static void main(String[] args) throws IOException {
         ArrayList<String> input = new ArrayList<>(Files.readAllLines(Path.of("src/com/company/adventofcode/day14.txt")));
-        Day14 d = new Day14();
-        d.solution(input.get(0), input.subList(2, input.size()));
+        new Day14().solution(input.get(0), input.subList(2, input.size()).stream().collect(Collectors.toMap(s -> s.substring(0, 2), s-> String.valueOf(s.charAt(6)))));
     }
 
-    private void solution(String polymerTemplate, List<String> subList) {
-        HashMap<String, String> input = new HashMap<>();
-        for (String s : subList) {
-            input.put(s.substring(0, 2), String.valueOf(s.charAt(6)));
-        }
+    private void solution(String polymerTemplate, Map<String, String> input) {
         HashMap<String, Long> pairs = new HashMap<>();
         for (int i = 0; i < polymerTemplate.length() - 1; i++) {
             Long val = pairs.getOrDefault(polymerTemplate.substring(i, i + 2), 0L);
@@ -36,24 +32,10 @@ public class Day14 {
             }
             pairs = tmp;
         }
-        HashMap<Character, Long> test = new HashMap<>();
-        for (Map.Entry<String, Long> entry : pairs.entrySet()) {
-            char a = entry.getKey().charAt(0);
-            Long valA = test.getOrDefault(a, 0L);
-            test.put(a, valA + entry.getValue());
-        }
-        Long high = 0L;
-        Long low = 999999999999999999L;
-        for (Map.Entry<Character, Long> e : test.entrySet()) {
-            if (e.getKey() == polymerTemplate.charAt(polymerTemplate.length() - 1)) {
-                high = Math.max(high, e.getValue()+1);
-                low = Math.min(low, e.getValue()+1);
-            } else {
-                high = Math.max(high, e.getValue());
-                low = Math.min(low, e.getValue());
-            }
-        }
-        System.out.println("Solution: " + (high - low));
+        Map<Character, Long> test = pairs.entrySet().stream().collect(Collectors.toMap(e -> e.getKey().charAt(0), Map.Entry::getValue, Long::sum));
+        char last = polymerTemplate.charAt(polymerTemplate.length()-1);
+        test.put(last, test.get(last)+1);
+        System.out.println("Solution: " + (test.values().stream().max(Long::compareTo).get() - test.values().stream().min(Long::compareTo).get()));
     }
 
 }
