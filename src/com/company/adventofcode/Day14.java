3e541a6b20c38046a04664451884d573b6bc2d2a
jeroenvanbaast <jeroenvanbaast@gmail.com>
2021-12-14 12:06:28 +0100

    Day14

--- src/com/company/adventofcode/Day14.java
+++ src/com/company/adventofcode/Day14.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day14 {
 
@@ -14,23 +15,17 @@ public class Day14 {
     }
 
     private void solution(String polymerTemplate, Map<String, String> input) {
-        HashMap<String, Long> pairs = new HashMap<>();
+        Map<String, Long> pairs = new HashMap<>();
         for (int i = 0; i < polymerTemplate.length() - 1; i++) {
             Long val = pairs.getOrDefault(polymerTemplate.substring(i, i + 2), 0L);
             pairs.put(polymerTemplate.substring(i, i + 2), val + 1L);
         }
         for (int step = 1; step <= 40; step++) {
-            HashMap<String, Long> tmp = new HashMap<>();
-            for (Map.Entry<String, Long> entry : pairs.entrySet()) {
-                String a = entry.getKey().charAt(0) + input.get(entry.getKey());
-                Long valA = tmp.getOrDefault(a, 0L);
-                tmp.put(a, valA + entry.getValue());
-
-                String b = input.get(entry.getKey()) + entry.getKey().charAt(1);
-                Long valB = tmp.getOrDefault(b, 0L);
-                tmp.put(b, valB + entry.getValue());
-            }
-            pairs = tmp;
+            pairs = Stream.of(
+            pairs.entrySet().stream().collect(Collectors.toMap(e ->  e.getKey().charAt(0) + input.get(e.getKey()), Map.Entry::getValue, Long::sum)),
+            pairs.entrySet().stream().collect(Collectors.toMap(e ->  input.get(e.getKey()) + e.getKey().charAt(1), Map.Entry::getValue, Long::sum)))
+                    .flatMap(map -> map.entrySet().stream())
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Long::sum));
         }
         Map<Character, Long> test = pairs.entrySet().stream().collect(Collectors.toMap(e -> e.getKey().charAt(0), Map.Entry::getValue, Long::sum));
         char last = polymerTemplate.charAt(polymerTemplate.length()-1);
